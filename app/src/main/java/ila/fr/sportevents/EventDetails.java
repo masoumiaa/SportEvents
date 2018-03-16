c3bacebc21fe3bae55eb933076ffbaa69303f8c4
Abdel <ghani.merzouk@gmail.com>
2018-03-16 16:09:54 +0100

    rajout sport event page

---
+++ app/src/main/java/ila/fr/sportevents/EventDetails.java
@@ -0,0 +1,18 @@
+package ila.fr.sportevents;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amerzouk on 16/03/18.
+ */
+
+public class EventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+}
