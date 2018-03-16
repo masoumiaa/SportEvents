3520cc49c80c6b6995b973bcd4066877d275742a
Unknown <g.brossault@hotmail.fr>
2018-03-16 14:04:35 +0100

    First version of the new event page

---
+++ app/src/main/java/ila/fr/sportevents/NewEventActivity.java
@@ -0,0 +1,17 @@
+package ila.fr.sportevents;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gbrossault on 08/02/18.
+ */
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
